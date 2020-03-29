230454287e2bdb3915c21623ab3a37eb5a10c9bd
David Batista <dbatista.t@gmail.com>
2020-03-29 15:14:45 -0400

    fix(first-iteration): fix NoUnderstand handler

--- src/decision-engine/handlers/no-understand-decision.handler.ts
+++ src/decision-engine/handlers/no-understand-decision.handler.ts
@@ -1,12 +1,12 @@
-import { CommandHandler, IQueryHandler, CommandBus } from '@nestjs/cqrs';
+import { CommandBus, CommandHandler, IQueryHandler } from '@nestjs/cqrs';
 import { NoUnderstandDecisionCommand } from '../commands/no-understand-decision.command';
-import { ProblemAnswerCommand } from '../../nlg/commands/problem-answer.command';
+import { NoUndestandAnswerCommand } from '../../nlg/commands/no-undestand-answer.command';
 
 @CommandHandler(NoUnderstandDecisionCommand)
 export class NoUnderstandDecisionHandler
   implements IQueryHandler<NoUnderstandDecisionCommand> {
   constructor(private readonly commandBus: CommandBus) {}
   execute(query: NoUnderstandDecisionCommand): Promise<any> {
-    return this.commandBus.execute(new ProblemAnswerCommand());
+    return this.commandBus.execute(new NoUndestandAnswerCommand());
   }
 }
--- src/nlg/nlg.module.ts
+++ src/nlg/nlg.module.ts
@@ -2,8 +2,13 @@ import { Module } from '@nestjs/common';
 import { ProblemAnswerHandler } from './handlers/problem-answer.handler';
 import { ReceiveAnswerHandler } from './handlers/receive-answer.handler';
 import { CqrsModule } from '@nestjs/cqrs';
+import { NoUnderstandAnswerHanlder } from './handlers/no-understand-answer.hanlder';
 
-export const CommandHandlers = [ProblemAnswerHandler, ReceiveAnswerHandler];
+export const CommandHandlers = [
+  ReceiveAnswerHandler,
+  ProblemAnswerHandler,
+  NoUnderstandAnswerHanlder,
+];
 
 @Module({
   imports: [CqrsModule],
